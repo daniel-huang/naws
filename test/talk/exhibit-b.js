1502cc6e3da07dd768e33c19c24687d0ed2d62e3
Ruben LZ Tan <soggie@gmail.com>
2014-10-11 11:58:49 +0800

    Fixed exhibit B PUSH-PULL example

--- test/talk/exhibit-b.js
+++ test/talk/exhibit-b.js
@@ -1,10 +1,29 @@
+/**
+ *
+ * - send: 1210 -----------
+ * - send: 1723 ----------|----
+ * - send: 1513 ----------|---|----
+ * ---- recv: 1723 <------|----   
+ * - send: 1164 ----------|------------
+ * - send: 1146 ----------|----------------
+ * - send: 1926 ----------|--------------------
+ * ---- recv: 1164        |
+ * - send: 1841 ----------|------------------------
+ * ---- recv: 1513        |
+ * - send: 1844 ----------|----------------------------
+ * - send: 1430 ----------|--------------------------------
+ * ---- recv: 1210 <-------
+ * ---- recv: 1926
+ * ---- recv: 1841
+ * ---- recv: 1146
+ * ---- recv: 1430
+ * ---- recv: 1844
+ */
 'use strict';
 
 var zmq   = require('zmq');
 var ran   = require('./random').ran;
 var color = require('./random').color2;
-var work  = require('./binarytree');
-//var ADDR  = 'tcp://127.0.0.1:13371';
 var ADDR = 'inproc://exhibit-b';
 
 var push  = zmq.socket('push');
@@ -22,19 +41,20 @@ pull3.on('message', handle(4));
 
 function handle(code) {
   return function (msg) {
-    process.nextTick(function () {
+    setTimeout(function () {
       msg = msg.toString();
-      if (code === 4) work();
       console.log('---- recv: ' + msg[color(code)]);
-    });
+    }, ran(5000, 500));
   }
 }
 
-push.bindSync(ADDR);
-setInterval(function () {
-  //var msg = color(ran(1000, 1000) + '');
+push.bind(ADDR);
+var intHandler = setInterval(function () {
   var msg = ran(1000, 1000) + '';
-
   console.log('- send: ' + msg);
   push.send(msg);
 }, 500);
+
+setTimeout(function () {
+  clearInterval(intHandler);
+}, 5000);
